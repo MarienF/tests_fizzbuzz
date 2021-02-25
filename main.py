7cb94b6d56e7fcfc8445168f033f5ce5f565b584
marienfarge <marien.farge@gmail.com>
2021-02-25 16:40:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+    print("Alice donne le nombre :")
+    numAlice = random.randrange(-10, 101)
+    print(int(numAlice))
+
+
+
+if __name__ == '__main__':
+    main()
