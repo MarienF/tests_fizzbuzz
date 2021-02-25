b7cc7bc57cf9a17507078f490257af470435516e
marienfarge <marien.farge@gmail.com>
2021-02-25 17:14:21 +0100

    should be negative failed

--- fizzbuuz_test.py
+++ fizzbuuz_test.py
@@ -6,3 +6,7 @@ class numTest(unittest.TestCase):
     def test_should_be_zero(self):
         actual = fizzbuzzchecker.is_bob(0)
         self.assertFalse(actual, None)
+
+    def test_should_be_negative(self):
+        actual = fizzbuzzchecker.is_bob(-1)
+        self.assertFalse(actual, None)
