f6a2ee7e116e39afce26311bc7c3d5dd20f116d6
marienfarge <marien.farge@gmail.com>
2021-02-25 16:53:43 +0100

    should be zero

---
+++ fizzbuuz_test.py
@@ -0,0 +1,8 @@
+import unittest
+from fizzbuzz_checker import fizzbuzzchecker
+
+class numTest(unittest.TestCase):
+
+    def test_should_be_zero(self):
+        actual = fizzbuzzchecker.is_bob(0)
+        self.assertFalse(actual, "zero")
---
+++ fizzbuzz_checker.py
@@ -0,0 +1,5 @@
+class fizzbuzzchecker:
+
+    @staticmethod
+    def is_bob(num):
+        return False
